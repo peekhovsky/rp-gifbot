df2f865c7f8b20d78508588ffff51a879d8e60fd
peekhovsky <peekhovsky@gmail.com>
2025-02-23 18:53:24 +0100

    upd version

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-from flask import Flask, request
+import os
 import telegram
 
-token = '7706047778:AAEIIci7_LNix0RArwaVKwvubSiPidpf69k'
-
-bot = telegram.Bot(token=token)
-app = Flask(__name__)
-
-@app.route('/HOOK', methods=['POST'])
-def webhook_handler():
-    if request.method == 'POST':
-        update = telegram.Update.de_json(request.get_json(force=True), bot=bot)
-        chat_id = update.effective_chat.id
-
-        if update.message:
-            text = update.message.text
-            if 'test' in text:
-                bot.send_message(chat_id=chat_id, text=f'Replying on a test message, message text=[{text}]')
-
-        return 'OK'
+def webhook(request):
+    bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        chat_id = update.message.chat.id
+        # Reply with the same message
+        bot.sendMessage(chat_id=chat_id, text=update.message.text)
+    return "ok"
